cd07cab6788bef658cbb77f64457b5f8914a1a8d
yu0814 <yu0814.2021@gmail.com>
2026-03-06 13:26:28 +0900

    何も変えてないけどテストでcommit

--- calculator.py
+++ calculator.py
@@ -1,13 +1,4 @@
-a = int(input('最初の数字は何ですか >>'))
-b = int(input('最後の数字は何ですか >>'))
-c = input('記号は何ですか +|-|*|/ >>')
-if c == '+':
-    print(a + b)
-elif c == '-':
-    print(a - b)
-elif c == '*':
-    print(a * b)
-elif c == '/':
-    print(a / b)
-else:
-    print('error')
+a = 1
+b = 2
+c = "/"
+print(a c b)
